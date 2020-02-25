67a246fcd17e9b84891d139b2f62bfae9072fef5
GarrettBrad <GarrettBrad@users.noreply.github.com>
2020-02-25 09:26:47 -0800

    Fixed the game from creating an object
    The game is a singleton

--- SkeletonAdventure/src/Main.cpp
+++ SkeletonAdventure/src/Main.cpp
@@ -8,21 +8,18 @@ int CALLBACK WinMain(
 	LPSTR CmdLine, 
 	int nCmdShow)
 {
-	Game game;
-
-	// I create this on the heap so we don't have a int doing nothing the entire run time
-	int* result = new int;
-	*result = game.Init(hInstance);
-
-	if (*result)
-		return *result;
-
-	delete result;
-
-	while (!game.ShouldClose())
+	// Created in its own scope so there isn't a int floating around the entire runtime	
 	{
-		game.Run();
+		int result = Game::Init(hInstance);
+
+		if (result)
+			return result;
 	}
 
-	return game.GetExitCode();
+	while (!Game::ShouldClose())
+	{
+		Game::Run();
+	}
+
+	return Game::GetExitCode();
 }
